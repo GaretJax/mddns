7b12dc7513f351bc4cd2a45895f1b3e7ee5d2320
Jonathan Stoppani <jonathan.stoppani@wsfcomp.com>
2014-06-09 16:58:31 +0200

    Public endpoint and arguments support.

--- mddns/scripts/server.py
+++ mddns/scripts/server.py
@@ -1,6 +1,21 @@
 from mddns.server import runserver
+import argparse
+
+
+def get_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-s', '--interval', type=int, default=10)
+    parser.add_argument('-t', '--token', default='/var/run/mddns/token')
+    parser.add_argument('-p', '--port', default=7253, type=int)
+    parser.add_argument('-i', '--interface', default='0.0.0.0')
+    parser.add_argument('ipfile')
+    parser.add_argument('endpoint')
+    return parser
 
 
 def main():
-    server_address = ('0.0.0.0', 7000)
-    runserver(server_address, 'ip.txt', 'token.txt', 10)
+    parser = get_parser()
+    args = parser.parse_args()
+    server_address = (args.interface, args.port)
+    runserver(server_address, args.ipfile, args.token, args.interval,
+              args.endpoint)
